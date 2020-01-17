5ed11308bc8a7934edb7d607b70598542a3795a9
Addison Klinke <addison@openalpr.com>
2020-01-16 21:03:46 -0700

    rename debian file with version and build number

--- make_deb.sh
+++ make_deb.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+# Copy files and create deb
 rm -rf debian/usr
 mkdir -p debian/usr/local/bin
 cp ./{bm,condense,count,lsd,pview,pypi,up} debian/usr/local/bin
 dpkg-deb --verbose --build debian/
+
+# Rename with version convention
+version=`dpkg-parsechangelog -ldebian/DEBIAN/changelog --show-field Version`
+build_number=`date "+%Y%m%d%H%M%S"`
+deb_name="bash-utils_${version}_${build_number}.deb"
+mv debian.deb $deb_name
